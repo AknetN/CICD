99f6f4a26a4f4904e4fc7446f431cf27ac101837
AknetN <aknet52@gmail.com>
2023-06-27 11:46:33 +0200

    add country field to user form

--- test_app/form.py
+++ test_app/form.py
@@ -4,3 +4,5 @@ class UserForm(forms.Form):
     username = forms.CharField(max_length=255)
     email = forms.EmailField()
     password = forms.CharField(max_length=155)
+    country = forms.CharField(max_length=255)
+    
